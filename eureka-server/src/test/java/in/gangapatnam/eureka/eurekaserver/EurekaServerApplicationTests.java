f890be1193a187bf283fc02584207de6347f0a6a
javab <javab@192.168.248.1>
2020-03-19 18:11:26 +0530

    adding eureka server project

---
+++ eureka-server/src/test/java/in/gangapatnam/eureka/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package in.gangapatnam.eureka.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
